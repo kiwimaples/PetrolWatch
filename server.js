153aa7728569deac1b66be6197b4cf5075780e1d
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-28 19:40:36 +1000

    Move to using st

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var filed = require('filed');
 var fs = require('fs');
 var app = express();
 var server = require('http').createServer(app);
+var st = require('st');
+var mount = st({
+  path: __dirname,
+  index: 'index.html',
+  cache: false
+});
 var FeedParser = require('feedparser');
 var JSONStream = require('JSONStream');
 var port = process.env.NODE_PORT || process.env.PORT || 3000;
@@ -18,19 +24,11 @@ app.get('/sample.json', function(req, res) {
     .pipe(res);
 });
 
-//index.html
-app.get('/', function(req, res) {
-  filed(__dirname + '/index.html').pipe(res);
-});
-
-//map.html
-app.get('/map.html', function(req, res) {
-  filed(__dirname + '/map.html').pipe(res);
-});
-
 app.get('/bundle.js', function(req, res) {
   require('browserify')(__dirname + '/index.js').bundle().pipe(res);
 });
+
+app.get('/*', mount);
 
 server.listen(port, function(err) {
   if (err) {
