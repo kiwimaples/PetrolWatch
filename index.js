96d505abc58a12d164325c43cee6d1b62e24d1b6
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-29 11:24:30 +1000

    Improve styling - implement as single page app

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var _ = require('underscore');
 var JSONStream = require('JSONStream');
 var priceTemplate = _.template('91 Lead is cheapest at <%= price %> in <%= address %>');
 
+var pageActions = {
+  home: [ getPriceInfo ],
+  map: [ getLocation ]
+};
+
 //auto detect location
 function getLocation() {
     if (navigator.geolocation) {
@@ -34,6 +39,16 @@ function displayPrice(data) {
     });
 }
 
+
+function loadCurrentPage() {
+  var section = location.hash.slice(1) || 'home';
+  var actions = pageActions[section] || [];
+
+  actions.forEach(function(action) {
+    action();
+  });
+}
+
 function showPosition(position) {
     console.log(position)
     // var lat = position.coords.latitude;
@@ -41,14 +56,14 @@ function showPosition(position) {
 
     var lat = -32.082959700000000000;
     var lon = 115.839983299999870000;
-    
+
     var markerlat = -32.070959800000000000;
     var markerlon = 115.843883299999970000;
 
     // create a map in the "map" div, set the view to a given place and zoom
-    var map = L.map('map').setView([lat,lon], 15);
     var line = linestring([[ lat, lon ], [ markerlat, markerlon ]]);
     var c = center(line);
+    var map = L.map('map').setView(c.geometry.coordinates, 15);
 
     // add an OpenStreetMap tile layer
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -68,18 +83,9 @@ function showPosition(position) {
         fillOpacity: 1
     }).addTo(map);
 
-    //add comment 
+    //add comment
     marker.bindPopup("<b>Your Destination!</b><br>Cnr Amherst Rd & Nicholson Rd, CANNING VALE </br> <b>Estimated Time:</b> 3 mins").openPopup();
 }
 
-switch (location.pathname) {
-    case '/map.html': {
-        getLocation();
-        break;
-    }
-
-    default: {
-        getPriceInfo();
-    }
-}
-
+loadCurrentPage();
+window.addEventListener('hashchange', loadCurrentPage);
